c82aa4cf6332e6bab88a5f374f9e4c0a13d5361d
Akram <akram.dakshana18@gmail.com>
2026-09-18 00:57:28 +0530

    Basic React.js Frontend added

---
+++ frontend/src/App.jsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div className="container py-4">
+      <h1>SmartClass</h1>
+      <p>AI-powered platform for teachers</p>
+    </div>
+  )
+}
+
+export default App
